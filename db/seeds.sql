c41f527f2e6019a01ab00541e346b47fd32781f5
Cesar Martinez <chernanma@msn.com>
2020-11-15 22:23:16 -0500

    Schema.sql, seeds.sql, Connection.js and orm.js have been created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) values ('Cheese');
+INSERT INTO burgers (burger_name) values ('Meat Lover');
+INSERT INTO burgers (burger_name) values ('Triple Bacon');
+INSERT INTO burgers (burger_name) values ('Maplewood Smoke');
+INSERT INTO burgers (burger_name) values ('Egg Bacon and Cheese');
